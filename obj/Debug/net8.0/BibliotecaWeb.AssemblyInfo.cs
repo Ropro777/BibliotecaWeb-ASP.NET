751cd32fe6607bf12451467d5eeef9eb4a2d5859
Ropro777 <servin13rodrigo@gmail.com>
2025-11-04 23:39:21 -0300

    Initial commit - Sistema Bibliotecario UNA

--- obj/Debug/net8.0/BibliotecaWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/BibliotecaWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f186542b01fd529814720e181628471cf36198f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6e65cafca37a86444d6f9c24363461602a17f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
